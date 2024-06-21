fbd811b46b708d93742f26d1d9c3cce61846f05e
lemon78787878 <wakuwaku.5611@gmail.com>
2024-06-21 11:41:10 +0900

    他のユーザーのプロトタイプ編集ページへ遷移できないよう設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, only: [:edit]
+  before_action :set_prototype, only: [:show, :edit]
+  before_action :check_owner, only: [:edit]
   
   def index
     @prototypes = Prototype.all
@@ -19,7 +20,6 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
@@ -31,7 +31,6 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
@@ -48,9 +47,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def check_owner
+    unless @prototype.user == current_user
+      redirect_to root_path
     end
   end
 
